7e6db522a50b8754e813b09a78702af853af6f9c
Nicholas Chittle <nichittle@gmail.com>
2014-10-29 00:11:07 -0400

    Alias Commit

--- src/matrix.js
+++ src/matrix.js
@@ -139,53 +139,15 @@ printMatrix = function(matrix) {
   }
 };
 
-function Canvas() {
-  this.canvas = document.getElementById('imageCanvas');
-  this.context = this.canvas.getContext('2d');
-}
-
-/*
- * Paints the canvas.
- */
-Canvas.prototype.drawMatrix = function(matrix, pixelWidth) {
-  this.clear();
-  var length = matrix.length;
-  this.canvas.width = pixelWidth * length;
-  this.canvas.height = pixelWidth * length;
-
-  this.context.fillStyle = "#2e2";
-  this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
-
-  for (var i = 0; i < length; ++i) {
-    for (var j = 0; j < length; ++j) {
-      if (matrix[i][j] == 1) {
-        this.context.fillStyle = "#000";
-        this.context.fillRect(i * pixelWidth, j * pixelWidth, pixelWidth, pixelWidth);
-      } else if (matrix[i][j] == 0) {
-        this.context.fillStyle = "#FFF";
-        this.context.fillRect(i * pixelWidth, j * pixelWidth, pixelWidth, pixelWidth);
-      }
-    }
-  }
-}
-
-/*
- * Clears the canvas.
- */
-Canvas.prototype.clear = function() {
-  this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-}
-
 createQRMatrix = function(version, input) {
   var length = getLength(version);
   var matrix = createMatrix(length, length);
-  matrix = addFinderPattern(0, 0, matrix);
-  matrix = addFinderPattern(length-7, 0, matrix);
-  matrix = addFinderPattern(0, length-7, matrix);
-  matrix = addSeparators(matrix);
+  addFinderPattern(0, 0, matrix);
+  addFinderPattern(length-7, 0, matrix);
+  addFinderPattern(0, length-7, matrix);
+  addSeparators(matrix);
   addAlignmentPatterns(version, matrix);
   addTimingPatterns(matrix);
-  printMatrix(matrix);
 
   var canvas = new Canvas();
   canvas.drawMatrix(matrix, 10);
