8da98bbdbad9e44914a2277ff4e5cfc7b22d7cfe
Nicholas Chittle <nichittle@gmail.com>
2014-11-02 16:13:13 -0500

    Alias Commit

--- src/decoder.js
+++ src/decoder.js
@@ -1,6 +1,4 @@
 displayDecodedQRCode = function(matrix) {
-  var canvas = new Canvas("imageCanvas2");
-  canvas.drawMatrix(matrix, 10);
 };
 
 getVersion = function(matrix) {
@@ -10,12 +8,5 @@ getVersion = function(matrix) {
 decode = function(matrix) {
   var version = getVersion(matrix);
   console.warn(version);
-};
-
-init = function init() {
-  createQRCode(helloWorld, ECL_L);
-  matrix = encode(helloWorld, ECL_L);
-  decode(matrix);
-  displayDecodedQRCode(matrix);
 };
 
